a14addde649188a2297e949fbef0ca60be2b0e65
Jeff2310 <jeff23101719@gmail.com>
2018-04-11 22:18:05 +0800

    temp

---
+++ Assets/Scripts/BoostRealmController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostRealmController : MonoBehaviour {
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<EnemyPlane>().SpeedModifier = 1.2f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<EnemyPlane>().SpeedModifier = 1.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/LeaderPlaneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class LeaderPlaneController : EnemyPlane
+{
+    public static DelegateHelper.GetController getController;
+    public static DelegateHelper.SetController setController;
+    
+    public LeaderPlaneController():base()
+    {
+        MaximumSpeed = 4.5f;
+        MaximumHealth = 2;
+    }
+}
